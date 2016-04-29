55b3b57033343c67d09116aeb5b0217b67d127fc
Ray Sinnema <rssinnema@yahoo.com>
2016-04-29 12:22:47 +0200

    Add test for simultaneous Fizz and Buzz

--- fizzbuzz/src/test/java/remonsinnema/blog/fizzbuzz/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/remonsinnema/blog/fizzbuzz/FizzBuzzTest.java
@@ -16,6 +16,16 @@ public class FizzBuzzTest {
     assertFizzBuzz("Fizz", 3);
     assertFizzBuzz("4", 4);
     assertFizzBuzz("Buzz", 5);
+    assertFizzBuzz("Fizz", 6);
+    assertFizzBuzz("7", 7);
+    assertFizzBuzz("8", 8);
+    assertFizzBuzz("Fizz", 9);
+    assertFizzBuzz("Buzz", 10);
+    assertFizzBuzz("11", 11);
+    assertFizzBuzz("Fizz", 12);
+    assertFizzBuzz("13", 13);
+    assertFizzBuzz("14", 14);
+    assertFizzBuzz("FizzBuzz", 15);
   }
 
   private void assertFizzBuzz(String expected, int n) {
