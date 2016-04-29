3cebb6925b47a588dc69c7116c8413d0cc23f91a
Ray Sinnema <rssinnema@yahoo.com>
2016-04-29 12:26:29 +0200

    Correctly handle simultaneous Fizz and Buzz

--- fizzbuzz/src/main/java/remonsinnema/blog/fizzbuzz/FizzBuzz.java
+++ fizzbuzz/src/main/java/remonsinnema/blog/fizzbuzz/FizzBuzz.java
@@ -9,10 +9,14 @@ public class FizzBuzz {
   private final Collection<Term> terms = Arrays.asList(new Term(3, "Fizz"), new Term(5, "Buzz"));
 
   public String get(int n) {
+    StringBuilder result = new StringBuilder();
     for (Term term : terms) {
       if (n % term.getValue() == 0) {
-        return term.getText();
+        result.append(term.getText());
       }
+    }
+    if (result.length() > 0) {
+      return result.toString();
     }
     return Integer.toString(n);
   }
