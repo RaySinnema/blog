db3b3cf967d3117a9f06438814297c353ef7c6dd
Ray Sinnema <rssinnema@yahoo.com>
2016-07-30 10:01:03 +0200

    Extract field

--- fizzbuzz-functional/src/main/java/remonsinnema/blog/fizzbuzz/FizzBuzzer.java
+++ fizzbuzz-functional/src/main/java/remonsinnema/blog/fizzbuzz/FizzBuzzer.java
@@ -5,6 +5,11 @@ import java.util.function.Function;
 
 public class FizzBuzzer implements Function<Integer, String> {
 
+  private final Function<Integer, String> replaceNumberWithStringRepresentation
+      = n -> Integer.toString(n);
+  private final Function<Integer, String> replaceNumberWithFizz
+      = n -> "Fizz";
+
   @Override
   public String apply(Integer n) {
     return numberReplacerFor(n).apply(n);
@@ -12,8 +17,8 @@ public class FizzBuzzer implements Function<Integer, String> {
 
   private Function<Integer, String> numberReplacerFor(Integer n) {
     return n == 3
-        ? i -> "Fizz"
-        : i -> Integer.toString(i);
+        ? replaceNumberWithFizz
+        : replaceNumberWithStringRepresentation;
   }
 
 }
